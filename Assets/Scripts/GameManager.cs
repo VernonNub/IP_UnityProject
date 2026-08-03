e3d45acd7729f565cd96002283a03cbb622b5a5e
VernonNub <129353181+VernonNub@users.noreply.github.com>
2026-08-03 23:50:38 +0800

    PlayerManager & GameManager
    Created base script for basic mechanics for player and game manager. Player manager includes, Raycasting and Sanity draining. GameManager includes stats updating between POVs and story data variables.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,56 @@
+using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager instance;
+
+    public PlayerManager playerManager;
+
+    [Header("GameInfos")]
+    public Dictionary<string, string> items = new Dictionary<string, string>()
     {
-        
+        {"Vape", "What is this interesting device? I hear it can make cool smoke out of it! *Increases your happiness by 5 for 1min and increases your addiction by 5"},
+    };
+
+    public int playerPOV = 1;
+    public Dictionary<int, Dictionary<string, float>> povStats = new Dictionary<int, Dictionary<string, float>>()
+    {
+        {1, new Dictionary<string, float>(){ {"Sanity", 100}, { "Addiction", 0 }, { "Happiness", 50 } } },
+    };
+
+    [Header("Story Progression")]
+    public int storyProgress = 0;
+    public Dictionary<string, string> storyEvents = new Dictionary<string, string>();
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ChangePOV()
     {
-        
+        //Updates the Dictionary (Saves the current player stats)
+        povStats[playerPOV]["Sanity"] = playerManager.playerSanity;
+        povStats[playerPOV]["Addiction"] = playerManager.playerAddiction;
+        povStats[playerPOV]["Happiness"] = playerManager.playerHappiness;
+
+        //Changes the POV
+        playerPOV = 1;
+
+        //Updates the playerManager
+        playerManager.playerSanity = povStats[playerPOV]["Sanity"];
+        playerManager.playerAddiction = povStats[playerPOV]["Addiction"];
+        playerManager.playerHappiness = povStats[playerPOV]["Happiness"];
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,16 +1,68 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Player Stats")]
+    public float playerHealth;
+    public float playerSanity = 100;
+    public float playerAddiction = 0;
+    public float playerHappiness = 50;
+
+    [Header("RayCast")] 
+    [SerializeField] float raycastLength = 3;
+    private int mask = (1 << 6);
+
+    [Header("Inventory")]
+    public List<string> inventory = new List<string>();
+
+    [Header("Components")]
+    private Camera playerCamera;
+    private CharacterController cc;
+    public GameObject checkPoint;
+
+    [SerializeField] InteractibleManager interactibleManager;
+
+    private void OnEnable()
     {
-        
+        playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+        cc = gameObject.GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleRayCast();
+        HandlePlayerSanity();
+    }
+    
+    void OnInteract()
+    {
+        HandleInteraction();
+    }
+
+    private void HandleRayCast()
+    {
+        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, raycastLength, mask))
+        {
+            interactibleManager = hit.collider.gameObject.GetComponent<InteractibleManager>();
+        }
+        else
+        {
+            interactibleManager = null;
+        }
+    }
+
+    private void HandleInteraction()
+    {
+
+    }
+
+    private void HandlePlayerSanity()
+    {
+        float sanityValue = (1 + 0.01f * (100 - playerHappiness)) * 0.1f * (playerAddiction);
+
+        playerSanity -= sanityValue * Time.deltaTime;
     }
 }
