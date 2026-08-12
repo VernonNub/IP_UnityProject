a5711eeef066a3b497eb2f0754fbeab3dfa8cf61
aer675 <s10273247@connect.np.edu.sg>
2026-08-13 00:08:34 +0800

    Add SC AI state

--- Assets/Scripts/CharacterManagers/StudentCouncilManager.cs
+++ Assets/Scripts/CharacterManagers/StudentCouncilManager.cs
@@ -10,15 +10,19 @@ public class StudentCouncilManager : AIManager
     public enum AiStates
     {
         Vaping,
-        Talking,
+        Talking, 
         Moving,
+        Scolding, 
+        Idle
     }
 
     //Add your
     public Dictionary<Vector3, string> aiAction = new Dictionary<Vector3, string>()
     {
         //Keep your Y axis 0 for everything
-        {new Vector3(4.77f,0f,-14.52f), "Talking"}
+        {new Vector3(4.77f,0f,-14.52f), "Talking"},
+        {new Vector3(10.23f,0f,-5.67f), "Scolding"},
+        {new Vector3(0f,0f,0f), "Idle"}
     };
 
     //Current AI State
@@ -66,7 +70,12 @@ public class StudentCouncilManager : AIManager
             case AiStates.Talking:
                 changeState = false;
                 break;
-
+            case AiStates.Scolding:
+                changeState = false;
+                break;
+            case AiStates.Idle:
+                changeState = false;
+                break;
         }
     }
 
